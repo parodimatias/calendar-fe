a013816e8c860a968e290d871e35f5bdd357c768
Matias Parodi <matias.parodi@outlook.com>
2022-09-14 11:31:12 -0300

    workbox finished

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { startLogout } from '../../actions/auth';
-
+import React from "react";
+import { Offline, Online } from "react-detect-offline";
+import { useDispatch, useSelector } from "react-redux";
+import { startLogout } from "../../actions/auth";
 export const Navbar = () => {
+  const dispatch = useDispatch();
+  const { name } = useSelector((state) => state.auth);
 
-    const dispatch = useDispatch();
-    const { name } = useSelector( state => state.auth );
+  const handleLogout = () => {
+    dispatch(startLogout());
+  };
 
-    const handleLogout = () => {
-        dispatch( startLogout() );
-    }
-
-    return (
-        <div className="navbar navbar-dark bg-dark mb-4">
-            <span className="navbar-brand">
-                { name }
-            </span>
-            
-            <button 
-                className="btn btn-outline-danger"
-                onClick={ handleLogout }
-            >
-                <i className="fas fa-sign-out-alt"></i>
-                <span> Salir</span>
-            </button>
-
-        </div>
-    )
-}
+  return (
+    <div className="navbar navbar-dark bg-dark mb-4">
+      <span className="navbar-brand">{name}</span>
+      <Online>
+        <span className="text-success">Online</span>
+      </Online>
+      <Offline>
+        <span className="text-danger">Offline - Petitions will be saved</span>
+      </Offline>
+      <button className="btn btn-outline-danger" onClick={handleLogout}>
+        <i className="fas fa-sign-out-alt"></i>
+        <span> Salir</span>
+      </button>
+    </div>
+  );
+};
